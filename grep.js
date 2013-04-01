d8d3a367d854079593eced159987206af18d0ea7
Ladinu Chandrasinghe <ladinu@gmail.com>
2013-03-31 19:01:07 -0700

    Fixed unhandled errors in tests

--- grep.js
+++ grep.js
@@ -16,8 +16,6 @@ function Grep(options) {
   var execOptions = options.execOptions || {}
   var buildArgs   = options.buildArgs   || function(a) { return [].concat(a) }
 
-  this.buildArgs = buildArgs;
-
   args = buildArgs(args);
 
   if (callback) {
