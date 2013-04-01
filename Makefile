b28e13cce6af63b3d6c648c614934f216abf229e
Ladinu Chandrasinghe <ladinu@gmail.com>
2013-03-31 18:55:53 -0700

    fixed unhandled errors in tests

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
-
 REPORTER = spec
 
 test:
 	@NODE_ENV=test ./node_modules/.bin/mocha \
 		--reporter $(REPORTER)
-test2:
-	@NODE_ENV=test node test/catch
-.PHONY: test test2
+
+.PHONY: test 
--- test/grep.test.js
+++ test/grep.test.js
@@ -24,12 +24,15 @@ var compareStreams = function(stream1, stream2, done) {
   var stream1Hash = crypto.createHash('sha1');
   var stream2Hash = crypto.createHash('sha1');
 
+  var lock1 = 0, lock2 = 0;
   stream1.on('error', function(err) {
-    done(err);
+    ++lock1;
+    if(lock1 === 1) done(err);
   });
 
   stream2.on('error', function(err) {
-    done(err);
+    ++lock2;
+    if(lock2 === 1) done(err);
   });
 
   var updateHash = function(callback) {
@@ -61,83 +64,82 @@ var compareStreams = function(stream1, stream2, done) {
 }
 
 
-//describe('#configure([options])', function() {
-//  
-//  it('should allow global grep config', function(done) {
-//    grep.configure({
-//        buildArgs: function(a) { return ['-n', '-m', '3', '-e', a, 'testFile0.txt'] }
-//      , execOptions: { cwd: __dirname }
-//    });
-//
-//    var find = grep('is');
-//    var file = readFile('testFile2.txt');
-//
-//    compareStreams(find, file, done);
-//  });
-//
-//  it('should be overidden in specefic cases when options are given', function(done) {
-//    var options = { execOptions: { cwd: __dirname } };
-//    var find    = grep(['-m', '3', '-e', 'is', 'testFile0.txt'], options);
-//    var file    = readFile('testFile3.txt');
-//
-//    compareStreams(find, file, done);
-//  });
-//
-//  it('should continue with global config', function(done) {
-//    var find = grep('is');
-//    var file = readFile('testFile2.txt');
-//
-//    compareStreams(find, file, done);
-//  });
-//});
-//
-//describe('#resetConfig()', function() {
-//
-//  it('should reset global config', function(done) {
-//    grep.configure({
-//        buildArgs: function(a) { return ['-n', '-m', '2', '-e', a, 'testFile0.txt'] }
-//      , execOptions: { cwd: __dirname }
-//    });
-//
-//    grep('is', function(err1, stdout1, stderr1) {
-//      grep.resetConfig();
-//      grep.configure({ execOptions: { cwd: __dirname } });
-//
-//      grep(['-e', 'is', 'testFile0.txt'], function(err2, stdout2, stderr2) {
-//        var hash1    = crypto.createHash('sha1');
-//        var hash2    = crypto.createHash('sha1');
-//
-//        hash1.update(stdout1);
-//        hash2.update(stdout2);
-//
-//        hash1 = hash1.digest('hex');
-//        hash2 = hash2.digest('hex');
-//
-//        if(hash1 !== hash2) done(); else done(new Error('global config reset failed'));
-//      });
-//    });
-//  });
-//});
+describe('#configure([options])', function() {
+  
+  it('should allow global grep config', function(done) {
+    grep.configure({
+        buildArgs: function(a) { return ['-n', '-m', '3', '-e', a, 'testFile0.txt'] }
+      , execOptions: { cwd: __dirname }
+    });
+
+    var find = grep('is');
+    var file = readFile('testFile2.txt');
+
+    compareStreams(find, file, done);
+  });
+
+  it('should be overidden in specefic cases when options are given', function(done) {
+    var options = { execOptions: { cwd: __dirname } };
+    var find    = grep(['-m', '3', '-e', 'is', 'testFile0.txt'], options);
+    var file    = readFile('testFile3.txt');
+
+    compareStreams(find, file, done);
+  });
+
+  it('should continue with global config', function(done) {
+    var find = grep('is');
+    var file = readFile('testFile2.txt');
+
+    compareStreams(find, file, done);
+  });
+});
+
+describe('#resetConfig()', function() {
+
+  it('should reset global config', function(done) {
+    grep.configure({
+        buildArgs: function(a) { return ['-n', '-m', '2', '-e', a, 'testFile0.txt'] }
+      , execOptions: { cwd: __dirname }
+    });
+
+    grep('is', function(err1, stdout1, stderr1) {
+      grep.resetConfig();
+      grep.configure({ execOptions: { cwd: __dirname } });
+
+      grep(['-e', 'is', 'testFile0.txt'], function(err2, stdout2, stderr2) {
+        var hash1    = crypto.createHash('sha1');
+        var hash2    = crypto.createHash('sha1');
+
+        hash1.update(stdout1);
+        hash2.update(stdout2);
+
+        hash1 = hash1.digest('hex');
+        hash2 = hash2.digest('hex');
+
+        if(hash1 !== hash2) done(); else done(new Error('global config reset failed'));
+      });
+    });
+  });
+});
 
 describe('grep([args], [options])', function() {
 
   before(setSettings);
 
-//  it('should be a duplex stream', function() {
-//    var find = grep(['-e', 'some pattern']);
-//
-//    assert.equal(true, find instanceof stream);
-//    assert.equal(true, find.writable && find.readable);
-//  });
-//
+  it('should be a duplex stream', function() {
+    var find = grep(['-e', 'some pattern']);
+
+    assert.equal(true, find instanceof stream);
+    assert.equal(true, find.writable && find.readable);
+  });
+
   it('should throw an error when incomplete arguments are given', function(done) {
     var find = grep(['-m', '-e', 'people', 'testFile0.txt']);
     var lock = 0;
 
     find.on('error', function() {
       ++lock;
-      if (lock === 1)
-	done();
+      if (lock === 1) done(); // Prevent multiple calls to done() while handeling errors
     });
 
     find.once('data', function() {
@@ -147,13 +149,11 @@ describe('grep([args], [options])', function() {
 
   it('should not throw an error when complete arguments are given', function(done) {
     var find = grep(['-m', '1', '-e', 'people', 'testFile0.txt']);
-
     var lock = 0;
 
     find.on('error', function(err) {
       ++lock;
-      if (lock === 1)
-	done(new Error(err));
+      if (lock === 1) done(new Error(err));
     });
 
     find.once('data', function() {
@@ -161,64 +161,63 @@ describe('grep([args], [options])', function() {
     });
   });
 
-//  it('should match regular grep output', function(done) {
-//    var options = {
-//        buildArgs: function(a) {return ['-n', '-e'].concat(a)}
-//      , execOptions: {cwd: __dirname}
-//    }
-//    var file = readFile('testFile0.txt');
-//    var find = grep('is', options);
-//
-//    compareStreams(readFile('testFile1.txt'), find, done)
-//    file.pipe(find);
-//  });
+  it('should match regular grep output', function(done) {
+    var options = {
+        buildArgs: function(a) {return ['-n', '-e'].concat(a)}
+      , execOptions: {cwd: __dirname}
+    }
+    var file = readFile('testFile0.txt');
+    var find = grep('is', options);
+
+    compareStreams(readFile('testFile1.txt'), find, done)
+    file.pipe(find);
+  });
 });
 
 
-//describe('grep([args], [options], callback)', function() {
-//  
-//  before(setSettings);
-//
-//  it('should be a destroyed stream', function() {
-//    var find = grep(['-e', 'some_pattern'], function(){});
-//
-//    assert.equal(false, find.readable);
-//    assert.equal(false, find.writable);
-//    assert.equal(true, find instanceof stream);
-//  });
-//
-//  it('should throw an error when incomplete arguments are given', function(done) {
-//    var callback = function(err, stdout, stderr) {
-//      if (err) done(); else done(new Error(err));
-//    }
-//    grep(['-m', '-e', 'people', 'testFile0.txt'], callback);
-//  });
-//
-//  it('should not throw errors when complete arguments are given', function(done) {
-//    var callback = function(err, stdout, stderr) {
-//      done(err);
-//    }
-//    grep(['-m', '1', '-e', 'people', 'testFile0.txt'], callback);
-//  });
-//
-//  it('should match regular grep output', function(done) {
-//    var options = {
-//        buildArgs: function(a) {return ['-n', '-e', a, 'testFile0.txt']}
-//      , execOptions: {cwd: __dirname}
-//    }
-//    grep('is', options, function(err, stdout, stderr) {
-//      var hash1    = crypto.createHash('sha1');
-//      var hash2    = crypto.createHash('sha1');
-//      var fileData = fs.readFileSync(getPath('testFile1.txt'));
-//
-//      hash1.update(stdout);
-//      hash2.update(fileData);
-//
-//      hash1 = hash1.digest('hex');
-//      hash2 = hash2.digest('hex');
-//
-//      if (hash1 === hash2) done(); else done(new Error('grep output does not match'));
-//    });
-//  });
-//});
-//
+describe('grep([args], [options], callback)', function() {
+  
+  before(setSettings);
+
+  it('should be a destroyed stream', function() {
+    var find = grep(['-e', 'some_pattern'], function(){});
+
+    assert.equal(false, find.readable);
+    assert.equal(false, find.writable);
+    assert.equal(true, find instanceof stream);
+  });
+
+  it('should throw an error when incomplete arguments are given', function(done) {
+    var callback = function(err, stdout, stderr) {
+      if (err) done(); else done(new Error(err));
+    }
+    grep(['-m', '-e', 'people', 'testFile0.txt'], callback);
+  });
+
+  it('should not throw errors when complete arguments are given', function(done) {
+    var callback = function(err, stdout, stderr) {
+      done(err);
+    }
+    grep(['-m', '1', '-e', 'people', 'testFile0.txt'], callback);
+  });
+
+  it('should match regular grep output', function(done) {
+    var options = {
+        buildArgs: function(a) {return ['-n', '-e', a, 'testFile0.txt']}
+      , execOptions: {cwd: __dirname}
+    }
+    grep('is', options, function(err, stdout, stderr) {
+      var hash1    = crypto.createHash('sha1');
+      var hash2    = crypto.createHash('sha1');
+      var fileData = fs.readFileSync(getPath('testFile1.txt'));
+
+      hash1.update(stdout);
+      hash2.update(fileData);
+
+      hash1 = hash1.digest('hex');
+      hash2 = hash2.digest('hex');
+
+      if (hash1 === hash2) done(); else done(new Error('grep output does not match'));
+    });
+  });
+});
